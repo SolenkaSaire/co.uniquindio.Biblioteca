22fe572f75d9169841512e57c2d0eae523acb5e9
SolenkaSaire <solenkam.sairec@uqvirtual.edu.co>
2021-09-20 19:30:13 -0500

    Se agregan mensajes y elementos al repositorio

--- src/co/uniquindio/prog2/biblioteca/aplicacion/Aplicacion.java
+++ src/co/uniquindio/prog2/biblioteca/aplicacion/Aplicacion.java
@@ -1,5 +1,32 @@
 package co.uniquindio.prog2.biblioteca.aplicacion;
+import javax.swing.JOptionPane;
 
-public class Aplicacion {
+import co.uniquindio.prog2.biblioteca.model.Biblioteca;
+/**
+	 * 
+	 * @author Solenka Saire
+	 *
+	 */
+	public class Aplicacion {
 
-}
+		/**
+		 * Metodo principal
+		 * 
+		 * @param args Argumentos de la linea de comandos
+		 */
+		public static void main(String args[]) {
+			String nombre;
+			int id, edad;		
+			boolean sexo=false;
+	        nombre = JOptionPane.showInputDialog("Ingrese el nombre");
+	        id =  Integer.parseInt(JOptionPane.showInputDialog("Ingrese su id"));
+	        edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su edad"));
+
+			Biblioteca miBiblioteca; 
+			miBiblioteca = new Biblioteca();
+		
+			
+		}
+	
+	
+	}
--- src/co/uniquindio/prog2/biblioteca/model/Biblioteca.java
+++ src/co/uniquindio/prog2/biblioteca/model/Biblioteca.java
@@ -1,5 +1,57 @@
 package co.uniquindio.prog2.biblioteca.model;
 
 public class Biblioteca {
+	/**
+	 * 
+	 * @author Solenka Saire
+	 *
+	 */
+	/**
+	 * Son los atributos 
+	 */
+	private int id, edad;
+	private String nombre;
+	private boolean sexo=false;
+
+	/**
+	 * Metodo modificador
+	 * @param base Es la base del triangulo
+	 */
+	public void id(int id) {
+		this.id = id;
+}
+	
+	public void edad(int edad) {
+		this.edad = edad;
+}
+	
+	public void nombre(String nombre) {
+		this.nombre = nombre;
+}
+	
+	public void sexo(boolean sexo) {
+		this.sexo = sexo;
+}
+	
+	/**
+	 * Permite calcular el area del triangulo
+	 * @return el area
+	 */
+	public double solicitarPrestamo() {
+		double area;
+		area = id;
+		return area;
+		
+	}
+	
+	public boolean entregarLibro() {
+		boolean area;
+		area = sexo;
+		return area;
+		
+	}
+	
+
+	
 
 }
